c8c10c7cea2e5e549fe533940554f9afd9ac0bf6
pijey <pjvilloud-bitbucket@yahoo.fr>
2018-03-13 13:44:53 +0100

    Ajout tests ManagerServiceTest

--- src/test/java/com/ipiecoles/java/java350/service/ManagerServiceIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/ManagerServiceIntegrationTest.java
@@ -6,12 +6,14 @@ import com.ipiecoles.java.java350.model.Technicien;
 import com.ipiecoles.java.java350.repository.ManagerRepository;
 import com.ipiecoles.java.java350.repository.TechnicienRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.HashSet;
 
 @RunWith(SpringRunner.class)
@@ -26,6 +28,12 @@ public class ManagerServiceIntegrationTest {
 
     @Autowired
     TechnicienRepository technicienRepository;
+
+    @Before
+    public void setUp(){
+        technicienRepository.deleteAll();
+        managerRepository.deleteAll();
+    }
 
     @Test
     public void testAddTechniciens(){
@@ -48,5 +56,40 @@ public class ManagerServiceIntegrationTest {
         Assertions.assertThat(finalTechnicien.getManager()).isEqualTo(finalManager);
     }
 
+    @Test
+    public void testAddTechnicienWithNoManager(){
+        //Given
+        Technicien technicien = new Technicien("Dupond", "Jacques", "T98765", null, 1000d, 2);
+        technicien = technicienRepository.save(technicien);
+
+        //When
+        try {
+            managerService.addTechniciens(6L, technicien.getMatricule());
+            Assertions.fail("Cela aurait du planter !");
+        }
+        catch (Exception e){
+            //Then
+            Assertions.assertThat(e).isInstanceOf(EntityNotFoundException.class);
+            Assertions.assertThat(e).hasMessage("Impossible de trouver le manager d'identifiant 6");
+        }
+    }
+
+    @Test
+    public void testAddTechnicienWithManagerIdNull(){
+        //Given
+        Technicien technicien = new Technicien("Dupond", "Jacques", "T98765", null, 1000d, 2);
+        technicien = technicienRepository.save(technicien);
+
+        //When
+        try {
+            managerService.addTechniciens(null, technicien.getMatricule());
+            Assertions.fail("Cela aurait du planter !");
+        }
+        catch (Exception e){
+            //Then
+            Assertions.assertThat(e).isInstanceOf(EntityNotFoundException.class);
+            Assertions.assertThat(e).hasMessage("Impossible de trouver le manager d'identifiant null");
+        }
+    }
 
 }
